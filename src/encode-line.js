534f4ff229206b6dfc21c36d778d168a15c821a7
Dmitry Hlebin <3708576@gmail.com>
2022-05-01 00:46:36 +0300

    add: solve of encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count = 1;
+  let newArray = [];
+  let counterOfArray = 0;
+  str = str.split('');
+  str.forEach(function callback(element, index, array) {
+    if (str[index] === str[index+1]){
+      count++
+    } else {newArray[counterOfArray]= [+count, String(str[index])]; count = 1; counterOfArray++}
+  });
+  newArray = newArray.join(",").replace(/,/g, "").replace(/[1]/g, "");
+  return newArray ;
 }
 
 module.exports = {
   encodeLine
 };
+
+  //Run-length encoding----------------------------------------------------------
+ // Start("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW")
+  // [[12,'W'],[1,'B'],[12,'W'],[3,'B'],[24,'W'],[1,'B'],[14,'W']]
+/*function Start (str){
+
+    let count = 1;
+    let newArray = [];
+    let counterOfArray = 0;
+    str = str.split('');
+    console.log(str)
+    str.forEach(function callback(element, index, array) {
+      if (str[index] === str[index+1]){
+        count++
+      } else {newArray[counterOfArray]= [+count, String(str[index])]; count = 1; counterOfArray++}
+    });
+    console.log(newArray)
+    return newArray 
+  }*/
