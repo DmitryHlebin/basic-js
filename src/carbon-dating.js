1001aebc8d9109edbdeb7fc914771ee5f415a969
Dmitry Hlebin <3708576@gmail.com>
2022-05-02 00:13:51 +0300

    add: solution of carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
+function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) != 'string' ||
+  sampleActivity == "" ||
+  isNaN(sampleActivity) ||
+  +sampleActivity <= 0 ||
+  +sampleActivity > 15
+  )
+  
+  {return false}
+  let age;
+  age = Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))/(0.693/HALF_LIFE_PERIOD))
+  if(age == Infinity) {return false}
+  console.log(age)
+  return age;
+};
+//dateSample("3")
 module.exports = {
   dateSample
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getSeason(date) {
+  if ( date == false ) 
+  {return 'Unable to determine the time of year!'};
 
+  let season = "";
+  if(!date) {
+    return 'Invalid date!';
+  }
+  if(typeof(date.getMonth) != "function"){
+    return 'Invalid date!'
+  }
+  let month = date.getMonth();
+  console.log(month)
+  
+  if (month <= 1 || month == 11){
+    season = "winter";
+  }
+  else if (month >= 2 && month <= 4) {
+    season = "spring";
+  }
+  else if (month >= 5 && month <= 7) {
+    season = "summer";
+  }
+  else if (month >= 8 && month <=10) {
+    season = "autumn";
+  } else {return 'Invalid date!'}
+  console.log(season)
+  return season
+}
 module.exports = {
   getSeason
 };
